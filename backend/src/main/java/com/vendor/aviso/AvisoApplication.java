05df4c5bf692e9be3e37f97b9b36820c65c85968
Andre Relhas <falconi879@gmail.com>
2022-10-13 11:38:58 -0300

    Criação do Projeto

---
+++ backend/src/main/java/com/vendor/aviso/AvisoApplication.java
@@ -0,0 +1,13 @@
+package com.vendor.aviso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vendor/aviso/AvisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendor.aviso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
