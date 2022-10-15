09994f8a4dd3fd14e00f8e84a0be63584b32b682
andre <falconi879@gmail.com>
2022-10-15 17:22:14 -0300

    EndPoint com busca por data

--- backend/src/main/java/com/vendor/aviso/services/SaleService.java
+++ backend/src/main/java/com/vendor/aviso/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.vendor.aviso.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.vendor.aviso.entities.Sale;
@@ -14,7 +18,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales(){
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDAte,Pageable pageable){
+		
+		LocalDate hoje = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? hoje.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDAte.equals("") ? hoje : LocalDate.parse(maxDAte);
+		return repository.findSales( min, max, pageable);
 	}
 }
