3399f5f40b68525cc45fa87f90cfd0d7c441c51f
andre <falconi879@gmail.com>
2022-10-15 12:06:09 -0300

    Criação da Entidade Vendas e configuração do banco H2 e incremento de dados da tabela sales

--- backend/src/main/java/com/vendor/aviso/AvisoApplication.java
+++ backend/src/main/java/com/vendor/aviso/AvisoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvisoApplication {
+public class AvisoApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvisoApplication.class, args);
